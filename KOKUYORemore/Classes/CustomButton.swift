e74268b143e27d3f82b543da192e19f1c8c5bfcc
well <well@server.local>
2015-09-21 09:14:39 +0900

    CreateCustomButton

--- KOKUYORemore/Classes/CustomButton.swift
+++ KOKUYORemore/Classes/CustomButton.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2015 welldesign. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class CustomButton: UIButton {
+    @IBInspectable var defaultBackgroundColor :UIColor?
+    @IBInspectable var selectedBackgroundColor :UIColor?
+    @IBInspectable var highlightedBackgroundColor :UIColor?
+    
+    @IBInspectable var textColor: UIColor?
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+        didSet {
+            layer.borderColor = borderColor.CGColor
+        }
+    }
+    
+    override var highlighted :Bool {
+        didSet {
+            if highlighted {
+                self.backgroundColor = highlightedBackgroundColor
+            }
+            else {
+                self.backgroundColor = defaultBackgroundColor
+            }
+        }
+    }
+    
+    override var selected :Bool {
+        didSet {
+            if selected {
+                self.backgroundColor = selectedBackgroundColor
+            }
+            else {
+                self.backgroundColor = defaultBackgroundColor
+            }
+        }
+    }
+}
